efa74ac8af402946df3693515ddb87d716cfadd5
SourceCode2 <davidesofronia@gmail.com>
2017-10-31 23:02:34 +0100

    lol

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <ctype.h> //
+
+int main(int argc, char* argv[]) {
+  /*== Inizializzazione variabili ==*/
+  std::string eq[2];
+  int matrice[2][2];
+
+  /*== Input dati ==*/
+  std::cout << "Inserisci la prima equazione: ";
+  std::getline(std::cin, eq[0]);
+  std::cout << "Inserisci la seconda equazione: ";
+  std::getline(std::cin, eq[1]);
+ // 2x+3y+1 = 0
+ // 1/2x+5g+5 = 0
+  for (size_t i = 0, x = 0; i < count; i++) {
+    switch (eq[x][i]) {
+      case '+':
+        if
+      case '-':
+    }
+  }
+    x++;
+  }
+
+}
